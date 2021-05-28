24e654820815da70df0eda85d3725dc5e962fdd0
bentleypark <p2c2kbf@gmail.com>
2021-05-28 23:11:50 +0900

    package name  수정

---
+++ ui-common/src/main/java/com/sullivan/common/ui_common/initializer/TimberInitializer.kt
@@ -0,0 +1,20 @@
+package com.sullivan.common.ui_common.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.sullivan.common.ui_common.BuildConfig
+
+import timber.log.Timber
+
+class TimberInitializer : Initializer<Unit> {
+
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        Timber.d("TimberInitializer is initialized.")
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> =
+        emptyList()
+}
---
+++ ui-common/src/main/java/com/sullivan/common/ui_common/navigator/Navigator.kt
@@ -0,0 +1,11 @@
+package com.sullivan.common.ui_common.navigator
+
+import android.content.Context
+
+interface LoginNavigator {
+    fun openLogin(context: Context)
+}
+
+interface ReservationNavigator {
+    fun openReservationHome(context: Context)
+}
