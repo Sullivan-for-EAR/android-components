626fdf9450b0acf4297eeb5ac1f329b8a54041f5
bentleypark <p2c2kbf@gmail.com>
2021-07-03 09:43:26 +0900

    Calendar ex 함수 추가

---
+++ ui-common/src/main/java/com/sullivan/common/ui_common/ex/CalendarDateEx.kt
@@ -0,0 +1,7 @@
+package com.sullivan.common.ui_common.ex
+
+import java.util.*
+
+fun Calendar.year() = get(Calendar.YEAR)
+fun Calendar.month() = get(Calendar.MONTH)
+fun Calendar.day() = get(Calendar.DAY_OF_MONTH)
