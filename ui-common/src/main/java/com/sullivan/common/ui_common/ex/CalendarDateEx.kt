dc39fce8e5eaa4ef71ce31b8258cb45665643ff0
bentleypark <p2c2kbf@gmail.com>
2021-07-03 20:55:09 +0900

    CalendarDate EX 함수 추가

--- ui-common/src/main/java/com/sullivan/common/ui_common/ex/CalendarDateEx.kt
+++ ui-common/src/main/java/com/sullivan/common/ui_common/ex/CalendarDateEx.kt
@@ -1,7 +1,67 @@
 package com.sullivan.common.ui_common.ex
 
+import android.widget.TextView
+import timber.log.Timber
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.*
 
 fun Calendar.year() = get(Calendar.YEAR)
 fun Calendar.month() = get(Calendar.MONTH)
 fun Calendar.day() = get(Calendar.DAY_OF_MONTH)
+
+fun Calendar.getCurrentDayOfName(): String {
+    val date = this.time
+    val simpleDateFormat = SimpleDateFormat("EEEE", Locale.getDefault())
+    return simpleDateFormat.format(date)
+}
+
+fun String.convertDate(): String {
+    val format = SimpleDateFormat("yyyyMMdd", Locale.getDefault())
+    val calendar = Calendar.getInstance()
+    try {
+        calendar.time = format.parse(this)!!
+
+    } catch (e: ParseException) {
+        Timber.e(e)
+    }
+    return "${calendar.get(Calendar.MONTH) + 1}월 ${calendar.get(Calendar.DAY_OF_MONTH)}일 ${
+        calendar.getCurrentDayOfName()
+    }"
+}
+
+fun String.getTimeInfo(): String {
+
+    val hour = this.substring(0, 2).toInt()
+    val minute = this.substring(2).toInt()
+
+    if (hour <= 12) {
+        if (hour <= 9) {
+            if (minute <= 9) {
+                return "오전 0$hour:0$minute"
+            } else {
+                return "오전 0$hour:$minute"
+            }
+        } else {
+            if (minute <= 9) {
+                return "오전 $hour:0$minute"
+            } else {
+                return "오전 $hour:$minute"
+            }
+        }
+    } else {
+        if (hour <= 9) {
+            if (minute <= 9) {
+                return "오후 0${hour - 12}:0$minute"
+            } else {
+                return "오후 0${hour - 12}:$minute"
+            }
+        } else {
+            if (minute <= 9) {
+                return "오후 ${hour - 12}:0$minute"
+            } else {
+                return "오후 ${hour - 12}:$minute"
+            }
+        }
+    }
+}
